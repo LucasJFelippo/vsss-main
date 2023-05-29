a471eed387f41952a58f79e944518b621e2841dd
Lucas de Felippe <lucasjfelippo@gmail.com>
2023-05-29 13:52:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from communication.main import Env
 
-env = Env()  # Env has as attributes ball, robots, field and as methods buildEnv
-env.buildEnv()  # buildEnv() calls FiraSim infos, build the objects and define the Env's attributes
+env = Env()
+env.buildEnv()
 
-env.ball  # Ball object has x,y,z as attributes, and a replace method that
+print(env.ball)
+print(env.robots)
+print(env.robots[0])
+print(env.robots[0].x)
+
+env.ball.replace(0.5, 0.5, 10, 20)
+
+robot1 = env.robots[0]
+robot1.on(5, 5)
