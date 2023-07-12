ebb9bf287f2c67d13650dc789f9369c674eb9bd0
LucasMartinsUthi <luscagbr@gmail.com>
2023-07-12 13:12:35 -0300

    📂 Move comn folder

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
+import threading
 import time
 
-from communication.main import Env
+from communication.classes.environment import Env
 
 
 env = Env()
-env.buildEnv()
-
-print(env)
-
-env.ball.replace(0.5, 0.5, 10, 20)
-
-robot1 = env.robots[0]
-print(robot1.x)
-robot1.on(5, 5)
-time.sleep(2)
-print(robot1.x)
